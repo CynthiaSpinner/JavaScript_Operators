c6c872f1367aad5737d7ca36b50aea9674edd312
Cynthia <CynthiaSpinner@gmail.com>
2025-03-25 17:16:56 -0500

    completed 3 exercises

--- app.js
+++ app.js
@@ -3,17 +3,50 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+let a = 20;
+let b = 4;
 
-// Exercise 2
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(dividing);
+
 console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
 
-// Exercise 3
+console.log("num + str:", num + str);
+console.log("num + str2:", num + str2);
+console.log("num + isPresent:", num + isPresent);
+console.log("firstName + num:", firstName + num);
+console.log("isPresent + str:", isPresent + str);
+console.log("firstName + lastName:", firstName + lastName);
+
 console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+console.log("val == str3:", val == str3);
+console.log("val === str3:", val === str3);
+console.log("!isAwake:", !isAwake);
+console.log('"eleven" == str4 && val >= str3:', "eleven" == str4 && val >= str3);
+console.log("!isAwake || isAwake:", !isAwake || isAwake);
+console.log("0 == false:", 0 == false);
+console.log("0 === false:", 0 === false);
+console.log("0 != false:", 0 != false);
+console.log("0 !== false:", 0 !== false);
